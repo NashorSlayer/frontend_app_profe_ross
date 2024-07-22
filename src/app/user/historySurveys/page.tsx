beb42ce274bcb045d26da66f04825ad215e0f5aa
NashorSlayer <arif.bordons@alumnos.ucn.cl>
2024-07-22 14:54:36 -0400

    ✨ feat: Add Routes for Graduates (answer) and in survey can preview and history

---
+++ src/app/user/historySurveys/page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+    return (
+        <div>Preview Page</div>
+    )
+}
+
+export default Page
---
+++ src/app/user/survey/preview/page.tsx
@@ -0,0 +1,9 @@
+
+
+const Page = () => {
+    return (
+        <div>Preview Page</div>
+    )
+}
+
+export default Page
