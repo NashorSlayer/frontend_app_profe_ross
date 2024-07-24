ed57b052f3386ef22050a36d8a0740e59edaa3a0
ScarletteAA <scarlette.araya@alumnos.ucn.cl>
2024-07-24 11:16:35 -0400

    feat: Refactor user page to use ShowUserHome component.

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,33 +1,9 @@
-"use client"
-import ButtonClick from "@/components/ButtonClick";
-import { useRouter } from "next/navigation";
+import ShowUserHome from "@/components/views/ShowUserHome";
 
 const userHomePage = () => {
-
-    const router = useRouter();
-
     return (
-        <div className="">
-            <h1 className="text-4xl font-bold text-center mt-10 text-white "
-            >User Home Page
-            </h1>
-            <div className="flex justify-center items-center min-h-screen p-5 space-x-4">
-                <ButtonClick
-                    text="Profile"
-                    onClick={() => router.push("/user/profile")}
-                />
-                <ButtonClick
-                    text="Create Survey"
-                    onClick={() => router.push("/user/survey/create")}
-                />
-                <ButtonClick
-                    text="See Surveys"
-                    onClick={() => router.push("/user/survey")}
-                />
-            </div>
-        </div>
+      <ShowUserHome />         
     )
 }
-
 export default userHomePage;
 
